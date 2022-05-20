a50da2d4022af8776ca8f7f1ca2f1af3aafa87af
davidshadow25 <dap.pereira@campus.fct.unl.pt>
2022-05-20 12:07:37 +0100

    Added some new functions.

---
+++ src/VideoPlayer/ColorMode.cpp
@@ -0,0 +1,8 @@
+//
+//  ColorMode.cpp
+//  PortugalPresidents
+//
+//  Created by David Pereira on 20/05/2022.
+//
+
+#include <stdio.h>
---
+++ src/VideoPlayer/ColorMode.h
@@ -0,0 +1,12 @@
+//
+//  ColorMode.h
+//  PortugalPresidents
+//
+//  Created by David Pereira on 20/05/2022.
+//
+
+#ifndef ColorMode_h
+#define ColorMode_h
+
+
+#endif /* ColorMode_h */
